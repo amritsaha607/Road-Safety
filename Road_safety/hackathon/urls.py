569747731f8159eb492168d8699b815c24074ca7
Amrit Saha <amritsaha607@gmail.com>
2020-08-27 10:40:58 +0530

    first-commit

---
+++ Road_safety/hackathon/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name='hackathon'
+
+urlpatterns=[
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('home/', views.PoliceHome, name='PoliceHome'),
+    path('logout/', views.logout, name='logout'),
+    path('record/', views.record, name='record'),
+    path('dumb/', views.dumb, name='dumb'),
+    path('take_driver/', views.take_driver, name='take_driver'),
+    path('mainserver/', views.mainserver, name='mainserver'),
+    path('drowsyHome/', views.drowsyHome, name='drowsyHome'),
+    path('recordDrowsy', views.recordDrowsy, name='recordDrowsy'),
+    path('redirectPageToRecordDrowsy/', views.redirectPageToRecordDrowsy, name='redirectPageToRecordDrowsy'),
+]
